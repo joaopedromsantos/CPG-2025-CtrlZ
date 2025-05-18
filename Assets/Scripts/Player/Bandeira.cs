22e21117964c5a1d50693218d2e5ccd8f172dfe5
j.santos <joaomartinscrf2019@gmail.com>
2025-05-18 05:53:55 -0300

    Fase1

--- Assets/Scripts/Player/Bandeira.cs
+++ Assets/Scripts/Player/Bandeira.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Bandeira : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Bandeira"))
         {
             SceneManager.LoadScene("History_2"); 
         }
